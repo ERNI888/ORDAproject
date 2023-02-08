f071aecf90bb09e5423107b5de3959d03a801b24
ERNI888 <yerniyaz.ashuov@fizmat,kz>
2023-02-08 13:17:02 +0600

    io

--- src/com/company/Dbfunctions.java
+++ src/com/company/Dbfunctions.java
@@ -34,4 +34,16 @@ public class Dbfunctions {
             System.out.println(e);
         }
     }
+
+    public void insert_row(Connection conn,String table_name,String name, String address){
+        Statement statement;
+        try {
+            String query=String.format("insert into %s(name,address) values('%s','%s');",table_name,name,address);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Registered");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,30 @@
 package com.company;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Dbfunctions db=new Dbfunctions();
+        Scanner scan = new Scanner(System.in);
+        Dbfunctions db =new Dbfunctions();
         Connection conn=db.connect_to_db("ordadb","postgres","12345678");
-        db.createTable(conn,"");
-        System.out.println('5');
+        //db.createTable(conn,"kokokoko");
+
+        System.out.println("Hello it's OrdaTour!");
+        System.out.println("Do you have account?");
+        System.out.println("1) Yes, log in");
+        System.out.println("2) No, create account");
+
+        int number = scan.nextInt();
+        if(number==1){
+            System.out.print("Your Name:");
+            String name = scan.nextLine();
+            System.out.println("\nYour Address:");
+            String address = scan.nextLine();
+            db.insert_row(conn,"kokokoko",name,address);
+        }
+        else{
+            System.out.println("OK");
+        }
     }
 }
