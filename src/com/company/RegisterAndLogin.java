2b66667005965edde9d0aa6ec3e5ba46d8120384
pinokiopin <asylbekabdukarimov527@gmail.com>
2023-02-17 13:20:18 +0600

    return process in RegisterAndLogin

--- src/com/company/RegisterAndLogin.java
+++ src/com/company/RegisterAndLogin.java
@@ -3,71 +3,85 @@ package com.company;
 import java.sql.Connection;
 import java.util.Scanner;
 
-public class RegisterAndLogin {
-     static Scanner scan = new Scanner(System.in);
-     static Dbfunctions db =new Dbfunctions();
-     static SearchByCountry sbc = new SearchByCountry();
-     static Connection conn=db.connect_to_db("postgres","postgres","pinokio");
-    public static void register(){
-        scan.nextLine();
+public class RegisterAndLogin extends User {
+    static Scanner scan = new Scanner(System.in);
+    static Dbfunctions db =new Dbfunctions();
+    static Connection conn= Dbfunctions.connect_to_db("postgres","postgres","pinokio");
+    public static void register() {
         System.out.print("\nREGISTRATION FORM:\n");
         System.out.print("Your Name:");
-        String name = scan.nextLine();
+        String name = scan.next();
         System.out.print("Your Surname:");
-        String surname = scan.nextLine();
+        String surname = scan.next();
         System.out.print("Create password:");
         int password = scan.nextInt();
         System.out.print("How much money do you have?:");
         int cash = scan.nextInt();
-        Dbfunctions.register(conn,"person",name,surname,password,cash);
+
+        User user = new User();
+        user.setName(name);
+        user.setSurname(surname);
+        user.setPassword(password);
+        user.setCash(cash);
+
+        Dbfunctions.register(conn, "person", user.getName(), user.getSurname(), user.getPassword(), user.getCash());
     }
 
-    public static boolean checkLogin(){
-//        scan.nextLine();
-        System.out.print("\nLOGIN FORM:\n");
-        System.out.print("Your Name:");
-        String name = scan.nextLine();
-        System.out.print("Your Surname:");
-        String surname = scan.nextLine();
-        System.out.print("Enter your password:");
-        Integer password = scan.nextInt();
+    public static int processHotelBooking(Connection conn, Scanner scan, String name, String surname, int password, int cashofperson) {
+        SearchByCountry.searchByCountry();
 
-        //Dbfunctions.login(conn,"person",name,surename,password);
-        boolean isLoginSuccessful = Dbfunctions.login(conn,"person",name,surname,password);
-        if (isLoginSuccessful) {
-            int cashofperson = Dbfunctions.get_cash_person(conn,"person",name,surname,password);
-            System.out.println("YOUR CASH NOW: " + cashofperson);
-            SearchByCountry.searchByCountry();
+        System.out.println("WRITE ID OF HOTEL:");
+        Integer write_id_hotel = scan.nextInt();
+        scan.nextLine();
+        int priceofhotel = Dbfunctions.get_price_hotel(conn, "tur", write_id_hotel);
+        System.out.println("THE PRICE OF THIS HOTEl: " + priceofhotel + " AND YOU HAVE:" + cashofperson);
 
-            System.out.println("WRITE ID OF HOTEL:");
-            Integer write_id_hotel = scan.nextInt();
-            scan.nextLine();
-            int priceofhotel = Dbfunctions.get_price_hotel(conn, "tur", write_id_hotel);
-            System.out.println("THE PRICE OF THIS HOTEl: " + priceofhotel + " AND YOU HAVE:" + cashofperson);
-
-            System.out.println("\nDO YOU CONFIRM YOUR HOTEL? YES==1/NO==2");
-            int confirm = scan.nextInt();
-            if(confirm == 1){
-                if(priceofhotel>cashofperson){
-                    System.out.println("YOU HAVE LESS MONEY, SORRY");
-                }
-                else if(priceofhotel<cashofperson){
-                    int remains = cashofperson - priceofhotel;
-                    db.update_cash(conn,"person", cashofperson, remains);
-                    System.out.println("YOU SUCCESSFULLY BOUGHT");
-                }
-                else {
-                    System.out.println("ERROR 404");
-                }
+        System.out.println("\nDO YOU CONFIRM YOUR HOTEL? YES==1/NO==2");
+        int confirm = scan.nextInt();
+        if (confirm == 1) {
+            if (priceofhotel > cashofperson) {
+                System.out.println("YOU HAVE LESS MONEY, SORRY");
+            } else if (priceofhotel < cashofperson) {
+                int remains = cashofperson - priceofhotel;
+                db.updateCash(conn, remains, cashofperson, name, surname, password);
+                System.out.println("YOU SUCCESSFULLY BOUGHT");
             } else {
-                System.out.println("Ok, see you soon");
+                System.out.println("ERROR 404");
+            }
+        } else if (confirm == 2) {
+            System.out.println("Choose again?:\n1)Yes==1\n2)No==2");
+            int choose = scan.nextInt();
+            switch (choose) {
+                case 1:
+                    processHotelBooking(conn, scan, name, surname, password, cashofperson);
+                case 2:
+                    System.out.println("Okay, see u soon!");
             }
         }
-        else{
-            System.out.println("ACCOUNT DOES NOT EXIST!\nMAYBE YOUR NAME/SURNAME/PASSWORD IS WRONG.");
+        return 0;
+    }
+
+    public static boolean checkLogin() {
+        System.out.print("\nLOGIN FORM:\n");
+        System.out.print("Your Name:");
+        String name = scan.next();
+        System.out.print("Your Surname:");
+        String surname = scan.next();
+        System.out.print("Enter your password:");
+        int password = scan.nextInt();
+
+        boolean isLoginSuccessful = Dbfunctions.login(conn, "person", name, surname, password);
+        if (isLoginSuccessful) {
+            int cashofperson = Dbfunctions.getCashAmountForPerson(conn, "person", name, surname, password);
+            System.out.println("YOUR CASH NOW: " + cashofperson);
+
+            processHotelBooking(conn, scan, name, surname, password, cashofperson);
+
+        } else {
+            System.out.println("MAYBE YOUR NAME/SURNAME/PASSWORD IS WRONG");
         }
         return isLoginSuccessful;
     }
-}
 
+}
 
