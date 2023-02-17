be40901cd97ea8dd25efaa8268520f391d8f3194
Izat <izat0011@gmail.com>
2023-02-17 13:03:11 +0600

    User and main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,17 @@
 package com.company;
-
-import java.sql.Connection;
-import java.util.Scanner;
-public class Main extends RegisterAndLogin {
-    static Scanner scan = new Scanner(System.in);
-    RegisterAndLogin rg = new RegisterAndLogin();
-    static Dbfunctions db = new Dbfunctions();
-    static SearchByCountry sbc = new SearchByCountry();
-    Connection conn = db.connect_to_db("postgres", "postgres", "12345678");
-
+import java.sql.Connection;import java.util.Scanner;
+public class Main {    static Scanner scan = new Scanner(System.in);
+    static RegisterAndLogin rg = new RegisterAndLogin();    static Dbfunctions db = new Dbfunctions();
+    static SearchByCountry sbc = new SearchByCountry();    Connection conn = db.connect_to_db("postgres", "postgres", "pinokio");
     public static void main(String[] args) {
-//        db.createTable(conn,"per");
-        System.out.println("Hello it's OrdaTour!");
-        System.out.println("Do you have account?");
-        System.out.println("1) Yes, log in");
-        System.out.println("2) No, create account");
+        System.out.println("Hello it's OrdaTour!");        System.out.println("Do you have account?");
+        System.out.println("1) Yes, log in");        System.out.println("2) No, create account");
 
-
-        int number = scan.nextInt();
-        scan.nextLine();
-        switch (number){
-            case 1:
-                checkLogin();
-                break;
-            case 2:
-                register();
-                checkLogin();
-                break;
-            default:
-                return;
-        }
-     }
+        int number = scan.nextInt();        scan.nextLine();
+        switch (number) {
+            case 1 : RegisterAndLogin.checkLogin();
+            case 2 : {                RegisterAndLogin.register();
+                RegisterAndLogin.checkLogin();            }
+            default : {            }
+        }     }
 }
-
--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,37 +1,21 @@
 package com.company;
-
-public class User {
-    public static String name;
-    public static String surname;
+public class User {    public static String name;
+    public static String surname;    public static int password;
     public static int cash;
-//
-//    public User() {
-//        this.name = name;
-//        this.surname = surname;
-//        this.cash = cash;
-//    }
-
     public String getName() {
-        return name;
-    }
-
+        return name;    }
     public void setName(String name) {
-        this.name = name;
-    }
-
+        this.name = name;    }
     public String getSurname() {
-        return surname;
-    }
-
+        return surname;    }
     public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
+        this.surname = surname;    }
     public int getCash() {
-        return cash;
-    }
-
+        return cash;    }
     public void setCash(int cash) {
-        this.cash = cash;
-    }
-}
+        this.cash = cash;    }
+    public void setPassword(int password) {
+        User.password = password;    }
+    public int getPassword() {
+        return password;
+    }}
